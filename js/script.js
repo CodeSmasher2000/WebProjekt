39372162efb0a95b94e5fa6156530ed42ecf71ef
CodeSmasher2000 <jonatanwahlstedt@gmail.com>
2017-03-07 17:48:46 +0100

    Fixat så man kan göra basic api-anrop

--- js/script.js
+++ js/script.js
@@ -1,31 +1,24 @@
 /* Anropar API och hämtar resultat som matchar den inmatade strängen */
 $("#submit-job").on("click", function(){
   var movieDiv = document.getElementById("div-job");
-  var baseUrl = "http://api.arbetsformedlingen.se/platsannons/matchning?lanid=";
+  var baseUrl = "http://api.arbetsformedlingen.se/platsannons/matchning";
   var jobInput = $("#select-job").find(":selected").val();
+  var lanInput = $("#select-lan").find(":selected").val();
   //$(movieDiv).show();
   //$(movieDiv).text("Laddar...");
   $.ajax({
-    url: baseUrl + encodeURI(jobInput),
+    url: baseUrl + "?lanid=" + encodeURI(lanInput) + "&nyckelord=" + encodeURI(jobInput),
     dataType: "JSON"
 
   }).done(function(data){
-    var movie = data.matchningslista;
-    
-	//$(movieDiv).text("");
-    //$("#title").show();
-    $.each(movie, function(index, movie) {
-      //$(movieDiv).append(movie.annonsrubrik);
-      console.log(movie.annonsrubrik);
-     
-      //var hr = document.createElement("HR");
-      //$(movieDiv).append(hr);
-       
-					
-			
-					
-                });
-   // console.log(movieList.length);
+    var jobresult = data.matchningslista;
+      console.log(jobresult);
+      console.log(jobresult.matchningdata);
+    var annons = jobresult.matchningdata;
+    var rubrik = annons[0].annonsrubrik;
+    console.log(rubrik);
+    $("#div-job").append('<p>' + rubrik  + '</p>')
+
   }).fail(function(data){
   //  $("#title").text("Något gick fel...");
 	//$("#title").show();
