4ae9a0518510c8c2c6fbd0964f770a4482fbe2c5
CodeSmasher2000 <jonatanwahlstedt@gmail.com>
2017-03-07 14:30:06 +0100

    la till dropdown meny
    ändra logo, headers i början, storlek på container

--- js/init.js
+++ js/init.js
@@ -1,8 +1,17 @@
+
+
+
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    
+    //dropdown
+      $(document).ready(function() {
+    $('select').material_select();
+  });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
